cbc1751241cd18fac7a3d31d6a641b6d072d29af
Jugal Kalal <jugalkalal04@gmail.com>
2017-04-12 04:13:40 +0530

    DataStructures

---
+++ datastructures/SegmentTree.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructures;
+
+/**
+ *
+ * @author Jugal Kalal
+ */
+class SegmentTree {
+    static int[] tree;
+    static int[] a;
+    
+    public static void buildtree(long a[]){
+    	//Height of segment tree
+        int x = (int) (Math.ceil(Math.log(a.length) / Math.log(2)));
+ 
+        //Maximum size of segment tree
+        int max_size = 2 * (int) Math.pow(2, x) - 1;
+        tree = new long[max_size]; // allocate memory
+        build(0,0,a.length-1,tree,a);
+    }
+    
+    public static void build(int node, int s, int e, long[] tree, long[] a) {
+        if (s == e) {
+            tree[node] = a[s];
+        } else {
+            int mid = (s + e) >> 1;
+            build(2 * node, s, mid, tree, a);
+            build(2 * node + 1, mid + 1, e, tree, a);
+            tree[node] = merge(tree[2 * node], tree[2 * node + 1]);
+        }
+
+    }
+    
+    public static void update(int node, int s, int e, int p, long[] tree, long[] a) {
+        if (s == e) {
+            tree[node]=a[p];
+            return;
+        }
+        int mid = (s + e) >> 1;
+        if (p <= mid) {
+            update(2 * node, s, mid, p, tree, a);
+        } else {
+            update(2 * node + 1, mid + 1, e, p, tree, a);
+        }
+        tree[node] = merge(tree[2 * node], tree[2 * node + 1]);
+    }
+
+    public static long query(int node, int s, int e, int l, int r, long[] tree, long[] a) {
+        if (s == l && e == r) {
+            return tree[node];
+        }
+        int mid = (s + e) >> 1;
+        if (r <= mid) {
+            //System.out.println(l+" "+r);
+            return query(2 * node, s, mid, l, r, tree, a);
+        }
+        if (l > mid) {
+            return query(2 * node + 1, mid + 1, e, l, r, tree, a);
+        }
+        return merge(query(2 * node, s, mid, l, mid, tree, a), query(2 * node + 1, mid + 1, e, mid + 1, r, tree, a));
+    }
+
+    public static long merge(long l, long r) {
+        return l + r;
+    }
+    
+}
